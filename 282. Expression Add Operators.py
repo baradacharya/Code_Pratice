eea50cba9dd812cc8b146d2f7a355d839de3a987
barada <barada.p.acharya@gmail.com>
2018-05-14 11:51:59 -0700

    May 14

---
+++ 282. Expression Add Operators.py
@@ -0,0 +1,58 @@
+class Solution(object):
+    def addOperators(self, num, target):
+        """
+        :type num: str
+        :type target: int
+        :rtype: List[str]
+        """
+    def addOperators(self, num, target):
+        res, self.target = [], target
+        for i in range(1,len(num)+1):
+            if i == 1 or (i > 1 and num[0] != "0"): # prevent "00*" as a number
+                self.dfs(num[i:], num[:i], int(num[:i]), int(num[:i]), res) # this step put first number in the string
+        return res
+
+    def dfs(self, num, temp, cur, last, res):
+        if not num:
+            if cur == self.target:
+                res.append(temp)
+            return
+        for i in range(1, len(num)+1):
+            val = num[:i]
+            if i == 1 or (i > 1 and num[0] != "0"): # prevent "00*" as a number
+                self.dfs(num[i:], temp + "+" + val, cur+int(val), int(val), res)
+                self.dfs(num[i:], temp + "-" + val, cur-int(val), -int(val), res)
+                self.dfs(num[i:], temp + "*" + val, cur-last+last*int(val), last*int(val), res)
+                # cur = 4+2 ,last = 2, val = 5 ,we have to do 4+2*5 then cur - last + last * val = 4+2 - 2 + 2 * 5
+
+s = Solution()
+print s.addOperators(num = "232", target = 8)
+
+# class Solution(object):
+#     def addOperators(self, num, target):
+#         """
+#         :type num: str
+#         :type target: int
+#         :rtype: List[str]
+#         """
+#         res = []
+#         self.target = target
+#         for i in range(1,len(num)+ 1):
+#             if i == 1 or (i > 1 and num[0]!= '0'):
+#                 self.dfs(num[i:],num[:i],int(num[:i]),int(num[:i]),res)
+#         return res
+#     #keep track of future array, expression uptil now, cur expression and last expression, res
+#     def dfs(self,num,exp,cur_value,last_value,res):
+#         if not num:
+#             if cur_value == self.target:
+#                 res.append(exp)
+#             return
+#
+#         for i in range(1,len(num)+1):
+#             val = num[:i]
+#             if i == 1 or (i > 1 and num[0] != '0'):
+#                 self.dfs(num[i:], exp + "+" + val, cur_value + int(val), int(val),res)
+#                 self.dfs(num[i:], exp + "-" + val, cur_value - int(val), -int(val), res)
+#                 self.dfs(num[i:], exp + "*" + val, cur_value - last_value + last_value * int(val), last_value * int(val), res)
+#                 # cur = 4+2 ,last = 2, val = 5 ,we have to do 4+2*5 then cur - last + last * val = 4+2 - 2 + 2 * 5
+
